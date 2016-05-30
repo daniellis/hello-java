d12e2748226d9a3c55e26151a37981180b4fab44
Christian <christianwilliams79@gmail.com>
2016-05-30 19:22:08 -0400

    adding even number test and .gitignore file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-}
+
+    System.out.format("%s is even? %s\n", "3", isEven(3));
+    System.out.format("%s is even? %s\n", "4", isEven(4));
+    System.out.format("%s is even? %s\n", "5", isEven(5));
+    System.out.format("%s is even? %s\n", "6", isEven(6));
+
+  }
+
+  public static String isEven(int num) {
+    return String.valueOf(num % 2 == 0);
+  }
 }
